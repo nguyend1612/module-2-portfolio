d6978e4f42c3baf8bb698f7025c29a76962dc134
David Nguyen <123915997+nguyend1612@users.noreply.github.com>
2023-03-27 22:30:46 -0500

    temporarily added project 1
    ill actually do the project and integrate project 1 properly later

---
+++ assets/script.js
@@ -0,0 +1,79 @@
+yturl = 'https://www.googleapis.com/youtube/v3/search?q='
+var youKey = '&key=AIzaSyB5faftGXgqJpv-C13VoHtm_UDWcPkovvs'
+//var title = 'movie';
+var key = '&apikey=9b055176'
+var titleEl = $('#our-form')
+//var titlebtnEl = $('#title-btn.pure-button')
+
+
+function handleFormSubmit(event) {
+  // Prevent the default behavior
+  event.preventDefault();
+
+  
+
+  var movie = document.querySelector('#title').value
+
+
+  if (movie == false) {
+    console.log('Please enter a movie title.');
+    return;
+  }
+  var queryString = 'https://www.omdbapi.com/?t=' + movie + key 
+  var youTube = 'https://www.googleapis.com/youtube/v3/search?q=' + movie +"trailer" + youKey ;
+  //location.assign('./search-results.html?q=' + queryString);
+  
+   fetch(queryString)
+   .then(function (response) {
+    return response.json();
+ })
+ .then(function (data) {
+    console.log(data);
+    document.getElementById('apiResponse').innerHTML = JSON.stringify(data);
+  });
+
+
+   fetch(youTube)
+   .then(function (response) {
+      return response.json();
+   })
+   .then(function (data) {
+      console.log(data);
+      
+    });
+
+  //console.log(movie); redundant
+  
+}
+
+//
+
+
+  titleEl.on('submit', handleFormSubmit);
+
+    $(document).ready(function(){
+        let apiKey = "AIzaSyB5faftGXgqJpv-C13VoHtm_UDWcPkovvs"
+     
+        $("form").submit((e) => {
+            e.preventDefault()
+            let search = $("#title").val() + "movie trailer"
+            videoSearch(apiKey,search,1)
+        })
+    })
+     
+    function videoSearch(apiKey,search,maxResults){
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + apiKey + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search,(data) => {
+            console.log(data)
+            
+            let video = ''
+     
+            data.items.forEach(item => {
+                video = `
+                <iframe width="420" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allowfullscreen></iframe>
+                `
+     
+                $("#videos").append(video)
+            });
+        })
+     
+    }
